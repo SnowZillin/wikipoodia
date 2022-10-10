a47cd0a783d8ea325f56148f724f682e43793557
SnowZillin <snowwolfie1@gmail.com>
2022-10-09 18:03:50 -0700

    Added all website pathing

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path('CSS', views.CSS, name='CSS'),
+    path('Django', views.Django, name='Django'),
+    path('Git', views.Git, name='Git'),
+    path('Python', views.Python, name='Python'),
+    path('HTML', views.HTML, name='HTML')
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 from . import util
 
 
@@ -7,4 +7,19 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def CSS(request):
+    return render(request, 'encyclopedia/CSS.html')
+
+def Django(request):
+    return render(request, 'encyclopedia/Django.html')
+
+def Git(request):
+    return render(request, 'encyclopedia/Git.html')
+
+def HTML(request):
+    return render(request, 'encyclopedia/HTML.html')
+
+def Python(request):
+    return render(request, 'encyclopedia/Python.html')
 
